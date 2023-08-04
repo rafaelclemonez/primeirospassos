18f8aa6baa3581b0b9878cdc0c3ad9b5f70ffde3
“rafaelclemonez” <rafaelc.abreu@hotmail.com>
2023-08-04 11:42:23 -0300

    mudeidnv

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ function toggleMode() {
 html.classList.toggle('light')
 const img = document.querySelector("#profile img")
 
+if(html.classList.contains ("light")) {
 //trocar a imagem
 img.setAttribute("src", "./avatarbranco.png")
-
+}
+else {
+  img.setAttribute("src", "/avatar.png")
+}
 //se tiver lightmode colocar a branca
 }
